79b9fa7c2ae461b9c4aeb84b1ab247f8f82905bd
Admin <pheonix97al@gmail.com>
2020-07-10 12:01:01 +0600

    Change

--- hr_employee_updation_update/models/hr_employee.py
+++ hr_employee_updation_update/models/hr_employee.py
@@ -3,16 +3,10 @@ from datetime import datetime, timedelta
 from odoo import models, fields, _, api
 from datetime import date
 
+
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    address_home_id = fields.Many2one(
-        'res.partner', 'Contact',
-        groups="hr.group_hr_user", tracking=True,
-        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
-    private_email = fields.Char(related='address_home_id.email', string="Private Email", groups="hr.group_hr_user")
-
-    home_address = fields.Char(related='address_home_id.email', string="Address")
     # additional fields
     bank_name = fields.Char(string="Name of the bank")
     iic = fields.Char(string="Individual identification code")
