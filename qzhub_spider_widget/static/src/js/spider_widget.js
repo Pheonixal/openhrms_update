e12f4390bdceee4b032000ba73c3d9e6e0d0cbcf
Admin <pheonix97al@gmail.com>
2020-07-30 11:14:41 +0600

    [ADD] Added min width to spider widget

--- qzhub_spider_widget/static/src/js/spider_widget.js
+++ qzhub_spider_widget/static/src/js/spider_widget.js
@@ -58,7 +58,8 @@ odoo.define('hr_employee_view.RadarWidget', function (require) {
                 this.$el.attr('style', this.nodeOptions.style);
                 this.$el.css({position: 'relative'});
                 let context = this.$canvas[0].getContext('2d');
-                context.canvas.style.maxWidth = '100%'
+                context.canvas.style.maxWidth = '100%';
+                context.canvas.style.minWidth = '100%';
                 this.chart = new Chart(context, spiderwidget);
                 let $value = $('<span class="oe_radar_widget_value">')
                 $value.css({
